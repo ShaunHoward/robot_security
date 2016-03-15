6df143038051a9561041366c4038e9e68ba4fc0b
Shaun Howard <smh150@case.edu>
2016-03-15 02:20:14 -0400

    added rospkg structure, moved python code to /src

---
+++ robot_security/src/robot_runner.py
@@ -0,0 +1,29 @@
+__author__ = 'shaun'
+
+
+# runs the robot(s) around the map on random grids
+
+# runs the robot with the given id string
+def run(robot_id):
+    pass
+
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
